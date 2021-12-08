fd87cd63a3269adbf92e87312d3d6698742e31a8
Andre Georges <andre.georges26373@hotmail.com>
2021-12-08 15:36:44 -0500

    pratique

---
+++ src/pratique.js
@@ -0,0 +1,20 @@
+
+
+
+
+export default function pratique () {
+
+
+
+    <div>Choisissez un nombre d'images a afficher
+    <form  >   
+        <select >  
+            <option value="defaut">choisissez dans le menu</option> 
+            <option value="3" id={0}>3</option> 
+            <option value="6" id={1}>6</option>
+            <option value="9"  id={2}>9</option>
+        </select>
+    </form>
+    </div>
+
+}
