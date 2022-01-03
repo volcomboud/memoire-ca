72baa29713f86643e55db0ad79afa8a8e9941a14
Andre Georges <andre.georges26373@hotmail.com>
2022-01-03 13:19:49 -0500

    return one random scientist

--- src/AfficherImages.js
+++ src/AfficherImages.js
@@ -1,18 +1,41 @@
 import { people } from './data.js';
-
+import React from 'react'
 
 export default function AfficherImages() {
+  
+  function getRandomInt(max){
+    return Math.floor(Math.random()*max); 
+  } //getRandomInt
 
-  const listItems = people.map(person =>
-    <div key={person.id}>
-      <img
-        src={person.imageId}
-        alt={person.name}
-        height={100}
-        
-      />
-    </div>
+    let idx = getRandomInt(people.length)
+ 
+    const randomScientist = people.filter(p =>
+      p.id === idx
+    );
+
+  const listItems = randomScientist.map(person =>
+            <>
+                        <img
+                          src={person.imageId}
+                          alt={person.name}
+                          height={100}
+                        />
+            </>
+
   );
   return <ul>{listItems}</ul>;
   
-}
+} //AfficherImages
+
+
+
+// const listItems = people.map(person =>
+//   <div key={person.id}>
+//     <img
+//       src={person.imageId}
+//       alt={person.name}
+//       height={100}
+//     />
+//   </div>
+// );
+// return <ul>{listItems}</ul>;
--- src/data.js
+++ src/data.js
@@ -1,38 +1,47 @@
 export const people = [{
     id: 0, // Used in JSX as a key
     name: 'Creola Katherine Johnson',
+    profession: 'chemist',
     imageId: 'https://i.imgur.com/MK3eW3A.jpg'
   }, {
     id: 1, // Used in JSX as a key
     name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
     imageId: 'https://i.imgur.com/mynHUSas.jpg'
   }, {
     id: 2, // Used in JSX as a key
     name: 'Mohammad Abdus Salam',
+    profession: 'chemist',
     imageId: 'https://i.imgur.com/bE7W1jis.jpg'
   }, {
     id: 3, // Used in JSX as a key
     name: 'Percy Lavon Julian',
+    profession: 'mathematician',
     imageId: 'https://i.imgur.com/IOjWm71s.jpg'
   }, {
     id: 4, // Used in JSX as a key
     name: 'Subrahmanyan Chandrasekhar',
+    profession: 'mathematician',
     imageId: 'https://i.imgur.com/lrWQx8ls.jpg'
   }, {
     id: 5, // Used in JSX as a key
     name: 'Lin Lanying',
+    profession: 'mathematician',
     imageId: 'https://i.imgur.com/1bX5QH6.jpg'
   }, {
     id: 6, // Used in JSX as a key
     name: 'Katsuko Saruhashi',
+    profession: 'mathematician',
     imageId: 'https://i.imgur.com/YfeOqp2s.jpg'
   }, {
     id: 7, // Used in JSX as a key
     name: 'Maria Skłodowska-Curie',
+    profession: 'mathematician',
     imageId: 'https://i.imgur.com/szV5sdGs.jpg'
   }, {
     id: 8, // Used in JSX as a key
     name: 'Gregorio Y. Zara',
+    profession: 'mathematician',
     imageId: 'https://i.imgur.com/7vQD0fPs.jpg'
   }
 
