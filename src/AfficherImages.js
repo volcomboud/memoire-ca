1285d081e51bb7fd23fc6236328a36187dcb7546
Andre Georges <andre.georges26373@hotmail.com>
2022-01-03 16:05:57 -0500

    number of images, doesnt work very well

--- src/AfficherImages.js
+++ src/AfficherImages.js
@@ -2,17 +2,41 @@ import { people } from './data.js';
 import React from 'react';
 
 
-export default function AfficherImages() {
+export default function AfficherImages({nbImages}) {
   
   function getRandomInt(max){
     return Math.floor(Math.random()*max); 
   } //getRandomInt
 
-    let idx = getRandomInt(people.length)
- 
-    const randomScientist = people.filter(p =>
-      p.id === idx
+  
+    let idx1 = getRandomInt(people.length)
+    let idx2 = getRandomInt(people.length)
+    let idx3 = getRandomInt(people.length)
+    let idx4 = getRandomInt(people.length)
+    let idx5 = getRandomInt(people.length)
+    let idx6 = getRandomInt(people.length)
+    let idx7 = getRandomInt(people.length)
+    let idx8 = getRandomInt(people.length)
+    let idx9 = getRandomInt(people.length)
+
+    let randomScientist
+    
+    let nbDImagesAAfficher = {nbImages};
+    
+    if (nbDImagesAAfficher ===3 ){
+    randomScientist = people.filter(p =>
+      p.id === idx1 || p.id === idx2 || p.id === idx3
     );
+  } else if (nbDImagesAAfficher ===6 ){
+    randomScientist = people.filter(p =>
+      p.id === idx1 || p.id === idx2 || p.id === idx3 ||  p.id === idx4 || p.id === idx5 || p.id === idx6
+    );
+  } else {
+    randomScientist = people.filter(p =>
+      p.id === idx1 || p.id === idx2 || p.id === idx3 ||  p.id === idx4 || p.id === idx5 || p.id === idx6 || p.id === idx7 || p.id === idx8 || p.id === idx9 
+    );
+  }
+
 
   const listItems = randomScientist.map(person =>
             <>
